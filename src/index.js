e61278726f7771c8e5947456610b0adb9c93828a
Lary <110429721+Laryk96@users.noreply.github.com>
2022-12-18 14:49:35 +0200

    add autoScroll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-import NewApiService from './fetchIMG';
+import NewApiService from './apiPixabay';
 import { refs } from './refs';
 import { renderCards } from './renderCards';
+// import autoScroll from './autoScroll';
 
 const apiServise = new NewApiService();
 const simpleLightbox = new SimpleLightbox('.gallery a', {
@@ -18,7 +19,6 @@ refs.moreImgButton.classList.add('hidden');
 
 async function onSubmitForm(e) {
   e.preventDefault();
-  refs.moreImgButton.classList.add('hidden');
   const inputValue = e.currentTarget.elements.searchQuery.value.trim();
 
   if (inputValue === '') {
@@ -32,12 +32,16 @@ async function onSubmitForm(e) {
   try {
     const images = await apiServise.featchImg();
     const amountElement = images.hits.length;
+    console.log(images.hits.length);
+    if (images.hits.length < 20) {
+      refs.moreImgButton.classList.add('hidden');
+    }
 
     if (amountElement === 0) {
       return Notiflix.Notify.failure('Nothing was found for your request');
     }
     Notiflix.Notify.info(`Hooray! We found ${images.total} images.`);
-    renderCards(images.hits);
+    await renderCards(images.hits);
     simpleLightbox.refresh();
     refs.moreImgButton.classList.remove('hidden');
   } catch (error) {
@@ -46,39 +50,27 @@ async function onSubmitForm(e) {
 }
 
 async function onLoadMore() {
-  refs.moreImgButton.setAttribute('disabled', true);
   apiServise.incrementPage();
 
   try {
     const images = await apiServise.featchImg();
-    const amountElement = images.hits.length;
-
-    if (amountElement === 0) {
-      refs.moreImgButton.classList.add('hidden');
-      return Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-
     await renderCards(images.hits);
-    await smoothScroll();
     await simpleLightbox.refresh();
-
-    refs.moreImgButton.classList.remove('hidden');
-    refs.moreImgButton.removeAttribute('disabled');
   } catch (error) {
     return console.log(error.message);
   }
 }
 
-function smoothScroll() {
-  setTimeout(() => {
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
-  }, 200);
-}
+const options = {
+  rootMargin: '100px',
+};
+
+const loadMore = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      onLoadMore();
+    }
+  });
+};
+const observer = new IntersectionObserver(loadMore, options);
+observer.observe(refs.moreImgButton);
